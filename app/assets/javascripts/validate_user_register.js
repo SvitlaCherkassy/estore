27da89a103172f9704033690164ed5dfc95ce33c
vova <volodimir.partytskyi@gmail.com>
2012-06-11 17:34:01 +0300

    added jasmine test

--- app/assets/javascripts/validate_user_register.js
+++ app/assets/javascripts/validate_user_register.js
@@ -1,7 +1,16 @@
-function password_val(string1) {
-  if ( string1.length <= 5 ){
+function password_val(password) {
+  if ( password.length <= 5 ){
     $(".error_mes_pass").text("Password is too short (minimum is 6 characters)");
   } else {
     $(".error_mes_pass").text("");
+  }
+};
+
+function IsValidEmail(email) {
+  var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  if (filter.test(email)) {
+    $(".error_mes_email").text("");
+  } else {
+    $(".error_mes_email").text("Invalid email");
   }
 };
--- spec/javascripts/validate_user_register_spec.js
+++ spec/javascripts/validate_user_register_spec.js
@@ -1,9 +1,25 @@
 describe("UserRegister", function() {
-  it("password field should be error message", function() {
+  it("password field should be have error message", function() {
     loadFixtures("new_user_form.html");
     password_val("123");
     expect($(".error_mes_pass")).toHaveText("Password is too short (minimum is 6 characters)");
+  });
+
+  it("password field should not have error message", function() {
+    loadFixtures("new_user_form.html");
     password_val("123123");
     expect($(".error_mes_pass")).toHaveText("");
+  });
+
+  it("password field should be have error message", function() {
+    loadFixtures("new_user_form.html");
+    IsValidEmail("example@mail.ru");
+    expect($(".error_mes_email")).toHaveText("");
+  });
+
+  it("password field should not have error message", function() {
+    loadFixtures("new_user_form.html");
+    IsValidEmail("examplebla.ru");
+    expect($(".error_mes_email")).toHaveText("Invalid email");
   });
 });
